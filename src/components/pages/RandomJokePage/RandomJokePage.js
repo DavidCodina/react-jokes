c1d29275b69a65dc36d04e5d96baf99f7a7c0a5d
David Codina <davidcodina@Davids-MacBook-Pro.local>
2021-06-10 10:14:13 -0600

    Built out Jokes page.

--- src/components/pages/RandomJokePage/RandomJokePage.js
+++ src/components/pages/RandomJokePage/RandomJokePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react'; 
+import React, { useState, useEffect }  from 'react'; 
 import { getData } from '../../../helpers/axios-helper';
 
 
@@ -6,12 +6,61 @@ import { getData } from '../../../helpers/axios-helper';
 
 
 export function RandomJokePage(props){
-
-
+  const [ error, setError ]     = useState(null);  // eslint-disable-line
+  const [ loading, setLoading ] = useState(false); // eslint-disable-line
+  const [ joke, setJoke ]       = useState(null);  // eslint-disable-line
+  
 
   useEffect(() => {
-    // ...
-  });
+    setLoading(true);
+    setError(null);
+
+    // https://httpstat.us/400
+    getData('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
+    .then(res => {
+      setLoading(false);
+      const joke = res.data;
+      setJoke(joke);
+
+      console.log(joke);
+    })
+
+    .catch(err => {
+      setLoading(false);
+      setError(err);
+      setJoke(null);
+      console.error(err);
+    });
+  }, []);
+
+
+  const renderJoke = () => {
+    if (error){
+      return <div className="text-red text-center" style={{ fontSize: 32, fontFamily: 'Creepster' }}>Something Went Wrong!!!</div>
+    }
+
+    if (loading){
+      return <div className="text-gray text-center" style={{ fontSize: 32 }}>Loading...</div>
+    }
+
+    if (joke){
+      if (joke?.type === 'twopart'){
+        return (
+          <div className="mb-5 mx-auto w-90 p-3 bg-light text-gray border border-gray rounded-4 shadow-sm" style={{ fontSize: 24 }}>
+            <p><strong>Set Up:</strong> { joke.setup }</p>
+            <p className="mb-0"><strong>Delivery:</strong> { joke.delivery }</p>
+          </div>
+          
+
+        );
+      }
+
+
+    }
+
+
+    return null;
+  };
  
 
   return (
@@ -23,6 +72,10 @@ export function RandomJokePage(props){
         <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
         <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
       </div>
+
+
+
+      { renderJoke() }
 
 
 
